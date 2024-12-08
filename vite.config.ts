1324bc7b99fc9de26f582f9fc62417c8952c874b
raze10339 <raze10339@yahoo.com>
2024-12-07 19:09:28 -0500

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import preact from '@preact/preset-vite';
+
 export default defineConfig({
   plugins: [preact()],
-})
+  server: {
+    host: '0.0.0.0', 
+    port: 3000,     
+  },
+  preview: {
+    host: '0.0.0.0', 
+    port: parseInt(process.env.PORT || '3000'), 
+  },
+});
